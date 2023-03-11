f2283b65a3577348f2311e6793c56c6c63d9a3db
MarikaKonturova <mashakonturova@gmail.com>
2023-03-11 10:54:54 +0300

    proj init config

---
+++ src/app/App.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+
+import { ReturnComponentType } from 'shared/types/ReturnComponentType';
+
+const App: FC = (): ReturnComponentType => {
+  return <div />;
+};
+
+export default App;
